11249f4772a3a5368f1b6317add180d021ee81cb
Vinicius <viniciuspontesdoliveira@gmail.com>
2022-06-06 13:32:07 -0300

    adicionando pagina

---
+++ index.php
@@ -0,0 +1,22 @@
+<html>
+	
+	<head> 
+	<title> meu site em php </title>
+	
+	</head>
+	
+	
+	<body>
+	
+		
+		<?php
+		
+			for ($i = 0; $i < 10; $i ++){
+				print ("linha de numero". $i . "<br />");
+				}
+				
+			?>
+			
+		</body>
+		
+	</html>
